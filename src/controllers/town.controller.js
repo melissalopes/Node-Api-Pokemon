5755de95e7b1f0eb9d71738bfe59b539b52b9155
Melissa Lopes <melissa.gouveia@gft.com>
2021-10-14 14:49:55 -0300

    Town controller.

---
+++ src/controllers/town.controller.js
@@ -0,0 +1,38 @@
+const GetTowns = (req, res) => {
+    try{
+        res.status(200).json({ message: "Todas as cidades consultadas com sucesso!"})
+    }catch(err){
+        res.status(404).json(err)
+    }
+}
+
+const getTownsById = (req,res) => {
+    try{
+        res.status(200).json({ message: "Cidade de id 1 encontrada com sucesso!"})
+    }catch(err){
+        res.status(404).json(err)
+    }
+}
+
+const postTown = (req,res) => {
+    try{
+        res.status(200).json({ message: "Nova cidade adicionada com sucesso!"})
+    }catch(err){
+        res.status(500).json(err)
+    }
+}
+
+const putTown = (req,res) => {
+    try{
+        res.status(200).json({ message: "Cidade de id 1 atualizada com sucesso!"})
+    }catch(err){
+        res.status(500).json(err)
+    }
+}
+
+module.exports = {
+    GetTowns,
+    getTownsById,
+    postTown,
+    putTown
+}
